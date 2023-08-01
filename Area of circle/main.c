b9ec82b41a90f56c560e514d47ab4f85de5419b0
TanQinYang <108197858+TanQinYang@users.noreply.github.com>
2023-08-01 18:01:05 +0200

    Add files via upload

---
+++ Area of circle/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+double r,c,a;
+scanf("%lf",&r);
+c= 2*3.1415926*r;
+a=3.1415926*r*r;
+printf("%.4f\n",c);
+printf("%.4f\n",a);
+return 0;
+}
+
+
